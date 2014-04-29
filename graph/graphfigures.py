d223fb6fa2330f008b8f6b1ccc9c9d0927da10f6
Yura Zhytskiy <systemsofdown@gmail.com>
2014-04-29 10:18:56 +0300

    add alpha vers of method Newton

--- graph/graphfigures.py
+++ graph/graphfigures.py
@@ -1,6 +1,8 @@
 from numpy import *
 from matplotlib.figure import Figure
 import parser2
+import parser
+import re
 
 
 class GraphFigures(Figure):
@@ -34,11 +36,44 @@ class GraphFigures(Figure):
         if equation_1 and equation_2:
             for i, equation in enumerate([equation_1, equation_2]):
                 self._make_equation(equation)
-                arr_result = []
-                formula = str(self.result_formula[i]['y']).replace('^', '**')
-                for x in self.arr:
-                    arr_result.append(eval(formula))
-                self.figure.plot(array(arr_result))
+                self.figure.plot(array(self.get_result_formula(i, 'y')))
 
-    def solution_equations(self, arr_equations, start, finish, step):
-        pass
+    def get_result_formula(self, index, unknown):
+        result = []
+        formula = str(self.result_formula[index][unknown]).replace('^', '**')
+        formula = re.sub('[a-zA-Z]+', 'x', formula)
+        for x in self.arr:
+            result.append(eval(formula))
+        return result
+
+    def solution_equations(self, x, y, exactness):
+        count = 0
+        step = 0.1
+        h = 4*step
+        arr = [[0, 0], [0, 0]]
+        res = [0, 0]
+
+        # while True:
+        for i in range(2):
+            formula_x = str(self.result_formula[i]['y']).replace('^', '**')
+            formula_back_x = re.sub('[a-zA-Z]+', 'back_x', formula_x)
+            formula_next_x = re.sub('[a-zA-Z]+', 'next_x', formula_x)
+
+            formula_y = str(self.result_formula[i]['x']).replace('^', '**')
+            formula_back_y = re.sub('[a-zA-Z]+', 'back_y', formula_y)
+            formula_next_y = re.sub('[a-zA-Z]+', 'next_y', formula_y)
+            x_previous = x
+            back_x = x - step
+            next_x = x + step
+            arr[i][0] = (eval(parser.expr(formula_next_x).compile()) - eval(parser.expr(formula_back_x).compile())) / (2*h)
+
+            y_previous = y
+            back_y = y - step
+            next_y = y + step
+            arr[i][1] = (eval(parser.expr(formula_next_y).compile()) - eval(parser.expr(formula_back_y).compile())) / (2*h)
+        if arr[0][0] + arr[0][1] < 1 and arr[1][0] + arr[1][1] < 1:
+            return x, y
+            # if abs(x - x_previous) < exactness and abs(y - y_previous):
+            #     break
+        count += 1
+        return 0, 0
--- graph/window.py
+++ graph/window.py
@@ -46,18 +46,31 @@ class GraphFrame(Frame):
 
         Label(self._panel, text='Введіть перше рівняння:').pack(side=TOP, fill=X)
         self._equation_1_entry = Entry(self._panel)
-        self._equation_1_entry.insert(0, 'x**2-2*x-y+0.5=0')
+        self._equation_1_entry.insert(0, 'x**2-2*y-y+0.5=0')
         self._equation_1_entry.pack(side=TOP, fill=X)
 
         Label(self._panel, text='Введіть друге рівняння:').pack(side=TOP, fill=X)
         self._equation_2_entry = Entry(self._panel)
         self._equation_2_entry.insert(0, 'x**2+4*y**2-4=0')
         self._equation_2_entry.pack(side=TOP, fill=X)
-
-        # Label(self._panel, text='Введіть початок:').pack(side=LEFT, fill=X, anchor=N)
-        # self._start_entry = Entry(self._panel)
-        # self._start_entry.pack(side=RIGHT, fill=X, anchor=N)
         Button(self._panel, text='Створити графік', command=self._create_graph).pack(fill=X)
+
+        Label(self._panel, text='Введіть x:').pack(side=TOP, fill=X, anchor=N)
+        self._x_entry = Entry(self._panel)
+        self._x_entry.insert(0, str(1.32))
+        self._x_entry.pack(side=TOP, fill=X, anchor=N)
+        Label(self._panel, text='Введіть y:').pack(side=TOP, fill=X, anchor=N)
+        self._y_entry = Entry(self._panel)
+        self._y_entry.insert(0, str(0.75))
+        self._y_entry.pack(side=TOP, fill=X, anchor=N)
+        Label(self._panel, text='Введіть точність:').pack(side=TOP, fill=X, anchor=N)
+        self._exactness_entry = Entry(self._panel)
+        self._exactness_entry.insert(0, str(0.001))
+        self._exactness_entry.pack(side=TOP, fill=X, anchor=N)
+        Button(self._panel, text='Розв’язати систему рівнянь', command=self._solution_equations).pack(fill=X)
+
+        self.info_label = Label(self._panel, text='')
+        self.info_label.pack(side=BOTTOM, fill=X)
 
     def _create_graph(self):
         equation1 = self._equation_1_entry.get()
@@ -68,3 +81,10 @@ class GraphFrame(Frame):
         self._graph.show()
         self._graph.get_tk_widget().pack(side=TOP, fill=BOTH, expand=0)
         self._graph._tkcanvas.pack(side=TOP, fill=BOTH, expand=0)
+
+    def _solution_equations(self):
+        x = float(self._x_entry.get())
+        y = float(self._y_entry.get())
+        exactness = float(self._exactness_entry.get())
+        new_x, new_y = self.figure.solution_equations(x, y, exactness)
+        self.info_label.config(text='x = %f; y = %f' % (new_x, new_y))
